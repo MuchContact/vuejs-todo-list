99c673e1ae858a5df878e0fce573fe826ffe543b
MuchContact <liuhengjiangis@foxmail.com>
2017-03-06 23:58:56 +0800

    introduce webpack and extract vue template

---
+++ public/assets/js/components/Home.vue
@@ -0,0 +1,10 @@
+<template>
+    <div class="container page-header">
+      <h1>Simple Vue.js To-do List App</h1>
+      <p class="lead">
+        Type text below and hit enter key to add new tasks. Click priority label on task to change it. Tasks sort by priority and when created.
+        <br>
+        <small> Demo uses browser's local storage - task list will persist between visits unless browser's cache is cleared.</small>
+      </p>
+    </div>
+</template>
