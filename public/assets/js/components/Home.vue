2f93bf974f2f83d6f8bd2c0b1de23dd52dd908b4
MuchContact <liuhengjiangis@foxmail.com>
2017-03-08 00:11:06 +0800

    render todo-list component inside Home component; change http grammar responding to vue-resource 0.1.17

--- public/assets/js/components/Home.vue
+++ public/assets/js/components/Home.vue
@@ -1,10 +1,24 @@
 <template>
-    <div class="container page-header">
-      <h1>Simple Vue.js To-do List App</h1>
-      <p class="lead">
-        Type text below and hit enter key to add new tasks. Click priority label on task to change it. Tasks sort by priority and when created.
-        <br>
-        <small> Demo uses browser's local storage - task list will persist between visits unless browser's cache is cleared.</small>
-      </p>
+    <div class="container">
+      <div class="container page-header">
+        <h1>Simple Vue.js To-do List App</h1>
+        <p class="lead">
+          Type text below and hit enter key to add new tasks. Click priority label on task to change it. Tasks sort by priority and when created.
+          <br>
+          <small> Demo uses browser's local storage - task list will persist between visits unless browser's cache is cleared.</small>
+        </p>
+      </div>
+      <div class="row">
+        <div class="col-sm-12">
+          <todo-list></todo-list>
+        </div>
+      </div>
     </div>
 </template>
+
+<script>
+import TODO from './TodoList.vue'
+import Vue from 'vue'
+
+Vue.component("todo-list", TODO);
+</script>
